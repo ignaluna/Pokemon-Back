3f26cccf24adcd7d1be8c37b9f353cca4ffbd68a
Igna Luna <ignaluna98pb@gmail.com>
2023-02-23 13:48:19 -0300

    deploy 7

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ import {Provider} from "react-redux"
 import store from "./Redux/store"
 import axios from "axios"
 
-axios.defaults.baseURL = "https://back-pokemon-m3c8.onrender.com/" || "http://localhost:3001";
+// axios.defaults.baseURL = "http://localhost:3001";
+axios.defaults.baseURL = "https://pokemoncba.up.railway.app/"
 
 ReactDOM.render(
   <Provider store={store}>
